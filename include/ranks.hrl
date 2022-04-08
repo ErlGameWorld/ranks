10f40487fdb0f6a71d58267ff0eb203ae39fc149
SisMaker <156736github>
2022-04-09 01:15:24 +0800

    ft: 初始化提交

--- include/ranks.hrl
+++ include/ranks.hrl
@@ -1,7 +1,13 @@
 -ifndef(RANKS_H_).
 -define(RANKS_H_, true).
 
-
+-record(rank_info, {
+   key :: integer()
+   , otherInfo :: term()
+   , score1 :: term()
+   , score2 :: term()
+   , scoreN :: term()
+}).
 
 
 -endif.
